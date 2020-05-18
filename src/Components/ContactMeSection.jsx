c61e2e9553537fd2ab7463f87d4cfe969d5dfd8d
danielmiranda <manieldiranda@gmail.com>
2020-05-17 22:28:48 -0700

    testing form

--- src/Components/ContactMeSection.jsx
+++ src/Components/ContactMeSection.jsx
@@ -59,32 +59,34 @@ class ContactMeSection extends Component {
                             {/*                type="submit">Send*/}
                             {/*        </button>*/}
                             {/*    </div>*/}
+                            {/*/!*</form>*!/*/}
+                            {/*<form name="contact" data-netlify="true">*/}
+                            {/*    <p>*/}
+                            {/*        <label>Your Name: <input type="text" name="name"/></label>*/}
+                            {/*    </p>*/}
+                            {/*    <p>*/}
+                            {/*        <label>Your Email: <input type="email" name="email"/></label>*/}
+                            {/*    </p>*/}
+                            {/*    <p>*/}
+                            {/*        <label>Your Role: <select name="role[]" multiple>*/}
+                            {/*            <option value="leader">Leader</option>*/}
+                            {/*            <option value="follower">Follower</option>*/}
+                            {/*        </select></label>*/}
+                            {/*    </p>*/}
+                            {/*    <p>*/}
+                            {/*        <label>Message: <textarea name="message"></textarea></label>*/}
+                            {/*    </p>*/}
+                            {/*    <p>*/}
+                            {/*        <button type="submit">Send</button>*/}
+                            {/*    </p>*/}
                             {/*</form>*/}
-                            <form name="contact" data-netlify="true">
-                                <p>
-                                    <label>Your Name: <input type="text" name="name"/></label>
-                                </p>
-                                <p>
-                                    <label>Your Email: <input type="email" name="email"/></label>
-                                </p>
-                                <p>
-                                    <label>Your Role: <select name="role[]" multiple>
-                                        <option value="leader">Leader</option>
-                                        <option value="follower">Follower</option>
-                                    </select></label>
-                                </p>
-                                <p>
-                                    <label>Message: <textarea name="message"></textarea></label>
-                                </p>
-                                <p>
-                                    <button type="submit">Send</button>
-                                </p>
-                            </form>
 
-                            <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+                            <form name="contact" data-netlify="true" netlify netlify-honeypot="bot-field" hidden>
                                 <input type="text" name="name"/>
                                 <input type="email" name="email"/>
                                 <textarea name="message"></textarea>
+                                                                    <button type="submit">Send</button>
+
                             </form>
                         </div>
                     </div>
