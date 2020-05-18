1f64824d19460eccf70ad1f7cd978e87ecd67b08
danielmiranda <manieldiranda@gmail.com>
2020-05-17 22:15:05 -0700

    testing form

--- src/Components/ContactMeSection.jsx
+++ src/Components/ContactMeSection.jsx
@@ -10,6 +10,27 @@ class ContactMeSection extends Component {
                     <div className={'titleContainer'}>
                         <h1 className={'portfolioHeading'}> Contact Me </h1>
                     </div>
+
+                    <form name="contact" method="POST" data-netlify="true">
+                        <p>
+                            <label>Your Name: <input type="text" name="name"/></label>
+                        </p>
+                        <p>
+                            <label>Your Email: <input type="email" name="email"/></label>
+                        </p>
+                        <p>
+                            <label>Your Role: <select name="role[]" multiple>
+                                <option value="leader">Leader</option>
+                                <option value="follower">Follower</option>
+                            </select></label>
+                        </p>
+                        <p>
+                            <label>Message: <textarea name="message"></textarea></label>
+                        </p>
+                        <p>
+                            <button type="submit">Send</button>
+                        </p>
+                    </form>
                     <div className={'portfolioDivider'}/>
                     <div className="row">
                         <div className="col-lg-8 mx-auto">
