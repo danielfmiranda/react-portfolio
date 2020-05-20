ee07dea73e0fa52bc41cbb17d9f0f27737338491
danielmiranda <manieldiranda@gmail.com>
2020-05-19 21:03:25 -0700

    linl

--- src/Components/HeroDiv.jsx
+++ src/Components/HeroDiv.jsx
@@ -1,74 +1,75 @@
-import React, {Component} from 'react';
-import Button from 'react-bootstrap/Button'
-import '../Css/heroDiv.css';
-import {motion} from "framer-motion"
-import {Link} from "react-scroll/modules";
-import Nav from "react-bootstrap/Nav";
+    import React, {Component} from 'react';
+    import Button from 'react-bootstrap/Button'
+    import '../Css/heroDiv.css';
+    import {motion} from "framer-motion"
+    import Nav from "react-bootstrap/Nav";
+    import {Link} from "react-scroll";
 
 
-class HeroDiv extends Component {
 
-    tryThis = () => {
-        document.getElementById('#portfolio').scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"})
+    class HeroDiv extends Component {
 
-    }
+        tryThis = () => {
+            document.getElementById('#portfolio').scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"})
 
-
-    render() {
-        return (
-            <div>
-
-                <div className={'heroDivContainer'} id={'#heroDiv'}>
-
-                    <div className={'heroDivContent'}>
+        }
 
 
-                        <div className={'contentText'}>
-                            <motion.div
-                                initial={{opacity: 0}}
-                                animate={{opacity: 1}}
-                                transition={{type: "spring", duration: 0.7, stiffness: 30}}
-                            >
+        render() {
+            return (
+                <div>
+
+                    <div className={'heroDivContainer'} id={'#heroDiv'}>
+
+                        <div className={'heroDivContent'}>
 
 
-                                <h1> Daniel Miranda </h1>
-
-
-                                <h5> I am a <b>full-stack</b> web developer based out of the East Bay Area.
-                                </h5>
-                                <p> Currently working under the Automation Team at Kaiser Permanente, I use the latest
-                                    technologies to build applications that aid Kaiser in our mission to provide
-                                    quality, affordable healthcare.</p>
-                                <p> Outside of work, I enjoy working on cars 🚗, cycling 🚴‍♂️, and spending time with
-                                    my younger
-                                    brother 👦🏻.
-
-                                </p>
-
-
-                                <Link to="#portfolio"
-                                      spy={true}
-                                      smooth={true}
-                                      duration={1500}
+                            <div className={'contentText'}>
+                                <motion.div
+                                    initial={{opacity: 0}}
+                                    animate={{opacity: 1}}
+                                    transition={{type: "spring", duration: 0.7, stiffness: 30}}
                                 >
 
 
-                                    <Button onClick={this.tryThis} className={'seeMyWorkButton'}
-                                            variant="outline-primary">See
-                                        my work</Button>
-                                </Link>
-                            </motion.div>
+                                    <h1> Daniel Miranda </h1>
+
+
+                                    <h5> I am a <b>full-stack</b> web developer based out of the East Bay Area.
+                                    </h5>
+                                    <p> Currently working under the Automation Team at Kaiser Permanente, I use the latest
+                                        technologies to build applications that aid Kaiser in our mission to provide
+                                        quality, affordable healthcare.</p>
+                                    <p> Outside of work, I enjoy working on cars 🚗, cycling 🚴‍♂️, and spending time with
+                                        my younger
+                                        brother 👦🏻.
+
+                                    </p>
+
+
+                                    <Link to="#portfolio"
+                                          spy={true}
+                                          smooth={true}
+                                          duration={1500}
+                                    >
+
+
+                                        <Button  className={'seeMyWorkButton'}
+                                                variant="outline-primary">See
+                                            my work</Button>
+                                    </Link>
+                                </motion.div>
+                            </div>
                         </div>
+
                     </div>
 
+
                 </div>
-
-
-            </div>
-        );
+            );
+        }
     }
-}
 
-HeroDiv.propTypes = {};
+    HeroDiv.propTypes = {};
 
-export default HeroDiv;
+    export default HeroDiv;
