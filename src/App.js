dea9374fa80710a87bc39d43246e9de915751250
danielmiranda <manieldiranda@gmail.com>
2020-05-26 20:08:21 -0700

    analytics to see portfolio views

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from './Components/Container';
-import ReactGA from "react-ga";
+import ReactGa from "react-ga";
 
-
-function    initializeReactGA() {
-        ReactGA.initialize('UA-167751693-1');
-        ReactGA.initialize('/');
-    }
-
-initializeReactGA();
 
 function App() {
+
+    function initializeReactGA() {
+        ReactGa.initialize('UA-167751693-1');
+        ReactGa.pageview('/');
+    }
+
+    initializeReactGA();
+
+
     return (
         <div className="App">
-            <Container />
+            <Container/>
         </div>
     );
 }
